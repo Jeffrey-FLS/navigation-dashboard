b8d5684908a632f2110b23b0bd7b82132be5452e
Jeffrey Paulino <jp7kode@gmail.com>
2020-07-24 20:25:17 -0400

    npm installed material-ui, creating foundation branch

---
+++ src/app/template/template.component.jsx
@@ -0,0 +1,13 @@
+// React Components
+import React from 'react';
+
+// Assets
+import './template.component.scss';
+
+const TemplatePage = () => {
+    return (
+        <h1>Hello From TemplatePage</h1>
+    );
+};
+
+export default TemplatePage;
